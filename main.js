bc28d0d910a25abcda8fc6e64138dafcf89dc9bd
ashimsharma337 <you@example.com>
2020-04-23 21:37:36 -0400

    finished javascript code

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ var hours = 0;
 var display = document.getElementById("clockDisplay");
 
 
+var displayseconds = 0;
+var displayminutes = 0;
+var displayhours = 0;
+
+var interval = "null";
+var status = "stopped";
+var startbtn = document.getElementById("startStop");
+var resetbtn = document.getElementById("reset");
 
 
 
@@ -20,8 +28,51 @@ function myStopwatch(){
 
     }
 
-   display.innerHTML = hours+":"+minutes+":"+seconds;
+    if(seconds<10){
+        displayseconds = "0"+seconds.toString();
+    }
+    else{
+        displayseconds = seconds;
+    }
+    if(minutes<10){
+        displayminutes = "0"+minutes.toString();
+    }
+    else{
+        displayminutes = minutes;
+    }
+    if(hours<10){
+        displayhours = "0"+hours.toString();
+    }
+    else{
+        displayhours = hours;
+    }
+
+   display.innerHTML = displayhours+":"+displayminutes+":"+displayseconds;
 }
 
-window.setInterval(myStopwatch, 10);
+
+function startStop(){
+
+    if(status === "stopped"){
+     
+    interval = window.setInterval(myStopwatch, 1000);
+    startbtn.innerHTML = "Stop";
+    status = "started";   
+    }
+    else{
+    window.clearInterval(interval);
+    startbtn.innerHTML = "Start";
+    status = "stopped";
+    
+    }
+}
+
+function reset(){
+    window.clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    display.innerHTML = "00:00:00";
+    startbtn.innerHTML = "Start";
+}
 
